0d0e00728b8273b78ea2e40081382a7ecfb06230
Evan McGrane <mcgrane480@gmail.com>
2022-01-05 17:32:26 +0000

    made fixes for deployment9

--- src/components/SignUpPage/SignUp.js
+++ src/components/SignUpPage/SignUp.js
@@ -212,7 +212,7 @@ const SignUp = ({ history }) => {
             try {
 
                 console.log(publicAddress)
-                const {data} = await axios.post("/api/users/publicAddress", {publicAddress, username, email, password}, config);
+                const {data} = await axios.post("https://alpha-baetrum.herokuapp.com/api/users/publicAddress", {publicAddress, username, email, password}, config);
                 setLoading(true);
                 setText("Please Verify Your Wallet!")
                 console.log(data)  
@@ -253,7 +253,7 @@ const SignUp = ({ history }) => {
                 handleSignMessage(publicAddress, nonce).then(async function(signature) {
     
                     console.log(signature)
-                    const {data} = await axios.post("/api/auth/register", {signature, nonce, publicAddress, username, email, password}, config);
+                    const {data} = await axios.post("https://alpha-baetrum.herokuapp.com/api/auth/register", {signature, nonce, publicAddress, username, email, password}, config);
                     console.log(data);
                     console.log(data.token)
                     setText("Success!")
